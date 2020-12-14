4d17235f7a2e30c91fd6c1e95a46a8fa99e0c48d
kwekuhammond <hammondkweku@gmail.com>
2020-12-14 17:35:56 +0000

    project upload

---
+++ comp_vision/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace comp_vision
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.xml_selector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(7, 35);
+            this.imageBox1.MaximumSize = new System.Drawing.Size(420, 370);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 370);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(427, 36);
+            this.imageBox2.MaximumSize = new System.Drawing.Size(420, 370);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(420, 370);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // xml_selector
+            // 
+            this.xml_selector.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.xml_selector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.xml_selector.FormattingEnabled = true;
+            this.xml_selector.Items.AddRange(new object[] {
+            "Frontal Face",
+            "Eyes"});
+            this.xml_selector.Location = new System.Drawing.Point(277, 430);
+            this.xml_selector.Name = "xml_selector";
+            this.xml_selector.Size = new System.Drawing.Size(142, 21);
+            this.xml_selector.TabIndex = 3;
+            this.xml_selector.SelectedIndexChanged += new System.EventHandler(this.xml_selector_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(119, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Raw Camera Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(516, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processed Frames From Camera";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(126, 430);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Select object to detect";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox1.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(448, 431);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Save Captured Objects";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(869, 477);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xml_selector);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Process";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.ComboBox xml_selector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ comp_vision/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO;
+using System.Diagnostics;
+
+namespace comp_vision
+{
+    public partial class Main : Form
+    {
+        VideoCapture capture_Video;
+        Image<Bgr, Byte> ImageFrame;
+        CascadeClassifier Classifier;
+        string xml_file = "haarcascade_frontalface_default.xml", path = @"C:\Users\" + Environment.UserName + @"\Documents\computer vision - image output\";
+        string xml_folder = @"C:\Users\" + Environment.UserName + @"\Documents\computer vision - xml_files\";
+        bool save_objs, files_set;
+
+        public Main()
+        {
+            InitializeComponent();
+            check_xml_files(); 
+        }
+
+        private void startCamera()
+        {
+            try
+            {
+                capture_Video = new VideoCapture(0);
+                Application.Idle += ProcessFrame;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (files_set)
+            {
+                try
+                {
+                    imageBox1.Image = capture_Video.QueryFrame();
+                    ImageFrame = capture_Video.QueryFrame().ToImage<Bgr, byte>();
+                    Classifier = new CascadeClassifier(xml_folder + xml_file);
+
+                    if (imageBox1.Image != null)
+                    {
+                        Image<Gray, byte> GrayFrame = ImageFrame.Convert<Gray, byte>().Clone();
+                        //GrayFrame.Flip(Emgu.CV.CvEnum.FlipType.Vertical);
+                        CvInvoke.EqualizeHist(GrayFrame, GrayFrame);
+
+                        var faces = Classifier.DetectMultiScale(GrayFrame, 1.15, 10);
+
+                        foreach (var face in faces)
+                        {
+                            ImageFrame.Draw(face, new Bgr(0, 255, 0), 2);
+                            //processOutputTextBox.AppendText("eye detected");
+                            if (save_objs)
+                                ImageFrame.Save(path + set_file_Names(path) + ".bmp");
+                        }
+                        imageBox2.Image = ImageFrame;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+                Application.Exit();
+        }
+
+        private void xml_selector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (xml_selector.SelectedIndex == 0)
+                xml_file = "haarcascade_frontalface_default.xml";
+            else
+                xml_file = "haarcascade_eye.xml";
+        }
+
+        private void create_directory(string path_string)
+        {
+            if(!Directory.Exists(path_string))
+                Directory.CreateDirectory(path_string);
+        }
+
+        private string set_file_Names(string folder_path)
+        {
+            int num_of_files;
+            string file_name = "";
+
+            num_of_files = Directory.GetFiles(folder_path).Length;
+            num_of_files += 1;
+            return file_name = "image" + num_of_files.ToString();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.Start(path);
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+                save_objs = true;
+            else
+                save_objs = false;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            create_directory(path);
+            create_directory(xml_folder);
+            xml_selector.SelectedIndex = 0;
+            startCamera();
+        }
+
+        void check_xml_files()
+        {
+            string[] xml = {"haarcascade_frontalface_default.xml", "haarcascade_eye.xml"};
+            foreach(string file in xml)
+            {
+                try
+                {
+                    if (!File.Exists(xml_folder + file))
+                    {
+                        if (MessageBox.Show(@"xml files not found. Copy all needed xml files to Document\computer vision - xml_files\", "Files Not found", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                        {
+                            files_set = false;
+                            return;
+                        }
+                    }
+                    else
+                        files_set = true;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
